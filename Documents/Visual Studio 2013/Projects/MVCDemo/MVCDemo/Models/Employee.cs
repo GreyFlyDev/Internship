2eceae6344d4616d2e60499c38f4d7929f1b125a
Greg Flynn <gflynn@covdnssrv.co.volusia.fl.us>
2016-04-21 11:58:20 -0400

    added Employee Model

---
+++ Documents/Visual Studio 2013/Projects/MVCDemo/MVCDemo/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public int EmpFName { get; set; }
+        public int EmpLName { get; set; }
+    }
+}
